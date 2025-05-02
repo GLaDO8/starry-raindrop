bf0918fee34148203dc08ba2fb9ab0418c3dd68c
Shreyas Gupta <shreyas.charming@gmail.com>
2025-05-02 16:05:47 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,6 @@ export const main = async () => {
       collectionId: process.env.RAINDROP_COLLECTION_ID,
       title: star.repo.full_name,
       link: star.repo.html_url,
-      excerpt: star.repo.description,
       tags: _([
         "github",
         star.repo.language || undefined,
